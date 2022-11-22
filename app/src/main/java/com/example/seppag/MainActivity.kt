4f293b9da46266b20ab915d073ddfceeafbd6bb8
Wodman43 <lopezquira10@gmail.com>
2022-11-22 16:01:22 -0500

    segunda activity

--- app/src/main/java/com/example/seppag/MainActivity.kt
+++ app/src/main/java/com/example/seppag/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.seppag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seppag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        var view=binding.root
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(view)
+
+
+        binding.btnIniciarSesion.setOnClickListener {
+            var intent = Intent(this, activity_requerimientos::class.java)
+            startActivity(intent)
+        }
     }
 }
