b3dc7900d9fcb0e5068c27658646f18123562cb3
Juan029 <118846074+Juan029@users.noreply.github.com>
2022-11-23 17:15:58 -0500

    lo

--- app/src/main/java/com/example/seppag/SplashActivity.kt
+++ app/src/main/java/com/example/seppag/SplashActivity.kt
@@ -2,9 +2,13 @@ package com.example.seppag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
     }
